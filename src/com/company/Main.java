29fc2387f2efe10fa97df5e5937a7d3110ac105a
izzy <isabella.palmer.2020@student.lhschools.org>
2020-03-04 14:16:14 -0500

    smooth moves

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        testOddness();
+    }
+
+    public static void testOddness() {
+
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+        int number;
+
+        System.out.println("insert #");
+        number=keyboard.nextInt();
+
+        System.out.println("number is odd:" + (number % 2 != 0));
+
+
+
+
+
+
     }
 }
